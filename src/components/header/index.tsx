978d0363aee889c401d83962d72bde37f6016a5d
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2024-08-01 10:59:22 -0300

    first commit

---
+++ src/components/header/index.tsx
@@ -0,0 +1,8 @@
+
+export function Header() {
+    return (
+        <div>
+            <h1>Componente header</h1>
+        </div>
+    )
+}
---
+++ src/pages/dashboard/new/index.tsx
@@ -0,0 +1,8 @@
+
+export function New() {
+    return (
+        <div>
+            <h1>Pagina Cadastrar carro</h1>
+        </div>
+    )
+}
