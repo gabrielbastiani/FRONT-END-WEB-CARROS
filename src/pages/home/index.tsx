fea83c836bf8e6a5749ff40be5cc9dea67f7f9e9
gabrielbastiani <gabriel.bastiani@hotmail.com.br>
2024-08-01 16:43:33 -0300

    webcarros

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -41,50 +41,6 @@ export function Home() {
                         </span>
                     </div>
                 </section>
-
-                <section className="w-full bg-white rounded-lg">
-                    <img
-                        className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
-                        src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2024/202407/20240730/bmw-x4-3-0-twinpower-gasolina-m40i-steptronic-wmimagem20424796540.webp?s=fill&w=552&h=414&q=60"
-                        alt="carro"
-                    />
-                    <p className="font-bold mt-1 mb-2 px-2">BMW 320i</p>
-
-                    <div className="flex flex-col px-2">
-                        <span className="text-zinc-700 mb-6">Ano 2016/2016 | 23.000 Km</span>
-                        <strong className="text-black font-medium text-xl">R$190.000</strong>
-                    </div>
-
-                    <div className="w-full h-px bg-slate-200 my-2"></div>
-
-                    <div className="px-2 pb-2">
-                        <span className="text-black">
-                            Caxias do Sul - RS
-                        </span>
-                    </div>
-                </section>
-
-                <section className="w-full bg-white rounded-lg">
-                    <img
-                        className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
-                        src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2024/202407/20240730/bmw-x4-3-0-twinpower-gasolina-m40i-steptronic-wmimagem20424796540.webp?s=fill&w=552&h=414&q=60"
-                        alt="carro"
-                    />
-                    <p className="font-bold mt-1 mb-2 px-2">BMW 320i</p>
-
-                    <div className="flex flex-col px-2">
-                        <span className="text-zinc-700 mb-6">Ano 2016/2016 | 23.000 Km</span>
-                        <strong className="text-black font-medium text-xl">R$190.000</strong>
-                    </div>
-
-                    <div className="w-full h-px bg-slate-200 my-2"></div>
-
-                    <div className="px-2 pb-2">
-                        <span className="text-black">
-                            Caxias do Sul - RS
-                        </span>
-                    </div>
-                </section>
             </main>
         </Container>
     )
